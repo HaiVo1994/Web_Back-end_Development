a7d60e3f54bf777f4ac92daf41ed29594c1ef62f
haivo1994 <vophihai1994@gmail.com>
2019-10-13 22:51:21 +0700

    Cong duong cheo chinh

--- Part2MangVaPhuongThucTrongJava/BaiTap/src/Bt6TinhTongCacSoODuongCheoChinh.java
+++ Part2MangVaPhuongThucTrongJava/BaiTap/src/Bt6TinhTongCacSoODuongCheoChinh.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Bt6TinhTongCacSoODuongCheoChinh {
     public static void main(String[] args) {
-        int length = 10;
+        int length = 0;
+        Scanner enter = new Scanner(System.in);
+        do{
+            System.out.print("Input matrix size: ");
+            length = enter.nextInt();
+        }while ((length<0) || (length>20));
         int[][] arr = createSquareMatrix(length);
         for (int i=0; i<length; i++){
             for (int j=0; j<length; j++){
@@ -13,9 +20,11 @@ public class Bt6TinhTongCacSoODuongCheoChinh {
 
     public static int[][] createSquareMatrix(int length){
         int[][] matrix = new int[length][length];
-        for (int row = 0; row < length; row++) {
-            for (int column = 0; column < length; column++) {
-                matrix[row][column] = (int) (Math.random() * 10);
+        Scanner enter = new Scanner(System.in);
+        for (int i=0; i<length; i++) {
+            for (int j = 0; j < length; j++) {
+                System.out.print("Input matrix [" + i + "][" + j + "]: ");
+                matrix[i][j] = enter.nextInt();
             }
         }
         return  matrix;
