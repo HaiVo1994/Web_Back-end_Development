896b6f2a62d629437baf2b10f2a4a1068b7bdc97
haivo1994 <vophihai1994@gmail.com>
2019-10-14 11:36:56 +0700

    Lop circle va lop cylinder

---
+++ Part4KeThua/src/Cylinder.java
@@ -0,0 +1,17 @@
+public class Cylinder extends Circle {
+    private double height;
+
+    public Cylinder(double radius, String color, double height) {
+        super(radius, color);
+        this.height = height;
+    }
+
+    public double getVolume(){
+        return this.height * super.getArea();
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + " height = " + this.height;
+    }
+}
